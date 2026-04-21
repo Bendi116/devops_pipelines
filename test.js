9acd7d533113abd9ee9916ccc0041c897ff7d8d4
Bendegúz <bendeguz.bakterhaz.com>
2026-04-21 15:06:31 +0200

    Fix: misspell in pipeline.yaml

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 console.log("Starting tests")
-setTimeout(()=> console.log("Waiitng 3 seconds ...",3000))
+setTimeout(() => console.log("Waiitng 3 seconds ...",3000))
 console.log("Tests completess")
